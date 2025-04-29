790ac497750895c42226b63fd40f898067e30a34
KhoaDinhNguyen <khoacode1305@gmail.com>
2025-04-28 22:22:56 -0700

    feat: add logout

--- client/src/components/UserComponents/UserNavigation/UserImageDropList/UserImageDropList.js
+++ client/src/components/UserComponents/UserNavigation/UserImageDropList/UserImageDropList.js
@@ -1,15 +1,20 @@
-import { NavLink } from "react-router";
+import { NavLink, useNavigate } from "react-router";
 
 import styles from "./UserImageDropList.module.css";
 
 function UserImageDropList() {
+  const navigate = useNavigate();
+
+  const onClickLogOut = () => {
+    navigate("/home/login");
+  };
   return (
     <div className={styles.rootContainer}>
       <ul className={styles.listContainer}>
         <li className={styles.listItem}>
           <NavLink to={"profile"}>Your profile</NavLink>
         </li>
-        <li className={styles.listItem}>
+        <li className={styles.listItem} onClick={onClickLogOut}>
           <p>Log out</p>
         </li>
       </ul>
