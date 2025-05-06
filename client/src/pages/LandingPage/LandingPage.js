b8ceddb4d91948a58df0a66387c281786895696f
KhoaDinhNguyen <khoacode1305@gmail.com>
2025-05-05 23:57:41 -0700

    feat: create landing page

---
+++ client/src/pages/LandingPage/LandingPage.js
@@ -0,0 +1,16 @@
+function LandingPage() {
+  return (
+    <div>
+      <div>
+        <h2>Connect with your friends around the world</h2>
+        <p>
+          Messages is a free social media application allowing users around the
+          world to communicate with their friends, family by directing messages,
+          sharing photos
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default LandingPage;
--- client/src/pages/Root.js
+++ client/src/pages/Root.js
@@ -1,13 +1,14 @@
 import { useEffect } from "react";
 import { useNavigate } from "react-router";
+import LandingPage from "./LandingPage/LandingPage";
 
 function Root() {
   const navigate = useNavigate();
 
-  useEffect(() => {
-    navigate("home");
-  });
-  return <></>;
+  // useEffect(() => {
+  //   navigate("home");
+  // });
+  return <LandingPage />;
 }
 
 export default Root;
